47b79fc714c3e55f20724145c969ea1bbcac14da
Camila Venialgo <camivenialgo3@gmail.com>
2026-04-15 17:44:57 -0300

    Ejercicio 1: variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,13 @@
+//nombre, edad, materia que estoy cursando
+const nombre = "Camila";
+const edad = 20;
+const materia = "Desarrollo de Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+for (let i = 0; i < 3 ; i++) {
+    contador = contador + 1;
+}
+
+console.log(`Valor final: ${contador}`);

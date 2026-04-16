5693af3bb819fb3010c703319192add16db9c5ad
Camila Venialgo <camivenialgo3@gmail.com>
2026-04-16 15:42:26 -0300

    Ejercicio 5: DOM input y botón

--- ejercicios/c03-javascript/ej5-dom-input/app.js
+++ ejercicios/c03-javascript/ej5-dom-input/app.js
@@ -4,8 +4,14 @@ const pantalla = document.querySelector('#resultado');
 
 boton.addEventListener('click', () => {
     const altura = Number(input.value);
-    let arbol = "";
 
+    if (input.value === "" || altura < 1) {
+        pantalla.innerHTML = "ERROR. EL NÚMERO INGRESADO DEBE SER MAYOR A 0";
+        pantalla.style.color = "red";
+        return;
+    }
+
+    let arbol = "";
     for (let i = 1; i <= altura; i++) {
         
         for (let j = 0; j < i; j++) {

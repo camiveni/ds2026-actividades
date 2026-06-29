61f82aab07e3231c78b2685ddcd3100f813d9c84
Camila Venialgo <camivenialgo3@gmail.com>
2026-06-28 22:10:03 -0300

    incorporo hooks y useFetch a la librería

---
+++ ejercicios/c11-hooks/src/components/Layout/Header.tsx
@@ -0,0 +1,20 @@
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export function Header() {
+    return (
+        <Navbar bg="light" expand="lg" className="bg-body-tertiary">
+            <Container fluid>
+                <Navbar.Brand as={Link} to="/">📚 Librería</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarNav" />
+                <Navbar.Collapse id="navbarNav">
+                    <Nav>
+                        <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+                        <Nav.Link as={Link} to="/catalogo">Catálogo</Nav.Link>
+                        <Nav.Link as={Link} to="/libros/nuevo">Nuevo Libro</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
